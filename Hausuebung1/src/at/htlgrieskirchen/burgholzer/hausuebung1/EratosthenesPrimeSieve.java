57da153fa0ad0f47a9b54295e348836df769802f
AlexBurgi <burgholzera190205@sus.htl-grieskirchen.at>
2023-09-18 12:00:13 +0200

    Hausuebung1 POS

---
+++ Hausuebung1/src/at/htlgrieskirchen/burgholzer/hausuebung1/EratosthenesPrimeSieve.java
@@ -0,0 +1,13 @@
+package at.htlgrieskirchen.burgholzer.hausuebung1;
+
+public class EratosthenesPrimeSieve implements PrimeSieve{
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void intPrimes() {
+
+    }
+}
---
+++ Hausuebung1/src/at/htlgrieskirchen/burgholzer/hausuebung1/PrimeSieve.java
@@ -0,0 +1,6 @@
+package at.htlgrieskirchen.burgholzer.hausuebung1;
+
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+    public void intPrimes();
+}
